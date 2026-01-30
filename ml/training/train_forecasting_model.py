b4361c2b02544f416c503f191fecb788546b27ba
Suchismita Sahu <95347254+Suchi-BITS@users.noreply.github.com>
2026-01-30 21:38:27 +0530

    Delete ml/training directory

--- ml/training/train_forecasting_model.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-
-def train(df):
-    X = df.drop(columns=["views"])
-    y = df["views"]
-    model = RandomForestRegressor()
-    model.fit(X, y)
-    return model
